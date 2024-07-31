bc6bdd22aadb340405c9b4767e0380b7c6bfcf90
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-07-31 15:55:39 +0300

    contacts

--- resources/views/layouts/website/contact_buttons/_whatsapp.blade.php
+++ resources/views/layouts/website/contact_buttons/_whatsapp.blade.php
@@ -26,6 +26,6 @@
     }
 </style>
 <a href="https://api.whatsapp.com/send?phone={{ websiteInfo_hlp('whats_up') }}" class="float" target="_blank">
-    <i class="fa fa-whatsapp my-float"></i>
+    <i class="fa fa-whatsapp my-float"> </i>
 </a>
 <!-- End whatsapp -->
