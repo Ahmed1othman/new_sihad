4351d3826be8f0611fa18dcd09cd621bf7d77e8c
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-08-03 17:14:06 +0300

    whatsapp button

--- resources/views/layouts/website1/master.blade.php
+++ resources/views/layouts/website1/master.blade.php
@@ -2,32 +2,32 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <link rel="icon" href="">
-        {!! SEOMeta::generate() !!}
-        {!! OpenGraph::generate() !!}
-        {!! Twitter::generate() !!}
-        {!! JsonLd::generate() !!}
+    {!! SEOMeta::generate() !!}
+    {!! OpenGraph::generate() !!}
+    {!! Twitter::generate() !!}
+    {!! JsonLd::generate() !!}
 
     @include('layouts.website1.header')
 
 
 </head>
 <body>
-    <!-- Start Preloader -->
-    <div class="preloader">
-        <div class="preloader-wave"></div>
-    </div>
-    <!-- End Preloader -->
-    @include('layouts.website1.navbar')
-    @yield('content')
+<!-- Start Preloader -->
+<div class="preloader">
+    <div class="preloader-wave"></div>
+</div>
+<!-- End Preloader -->
+@include('layouts.website1.navbar')
+@yield('content')
 
-   @include('layouts.website.contact_buttons._whatsapp')
-    @include('layouts.website.contact_buttons._call')
-    @include('layouts.website1.footer')
-    @include('layouts.website1.footer-script')
+@include('layouts.website.contact_buttons.whatsapp')
+@include('layouts.website.contact_buttons.call')
+@include('layouts.website1.footer')
+@include('layouts.website1.footer-script')
 </body>
 
 </html>
