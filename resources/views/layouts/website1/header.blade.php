a4421ecdf11096f3b252be4a4efc4d0351be6f32
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-08-03 17:27:31 +0300

    whatsapp button

--- resources/views/layouts/website1/header.blade.php
+++ resources/views/layouts/website1/header.blade.php
@@ -56,6 +56,7 @@
             transform: rotate(-90deg);
         }
     </style>
+
 @endif
 
 
@@ -217,4 +218,56 @@
 
 
 
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<style>
+    .float{
+        position:fixed;
+        width:60px;
+        height:60px;
+        bottom:50px;
+        right:50px;
+        background-color:#25d366;
+        color:#FFF;
+        border-radius:50px;
+        text-align:center;
+        font-size:30px;
+        box-shadow: 2px 2px 3px #999;
+        z-index:100;
+    }
+
+    .my-float{
+        margin-top:16px;
+    }
+
+    .my-float i:hover{
+        margin-top:16px;
+        color:#5cd08d;
+    }
+</style>
+<!-- ======= whatsapp ======= -->
+<style>
+    .float1{
+        position:fixed;
+        width:60px;
+        height:60px;
+        bottom:50px;
+        left:50px;
+        background-color: #fc0000;
+        color:#FFF;
+        border-radius:50px;
+        text-align:center;
+        font-size:30px;
+        box-shadow: 2px 2px 3px #999;
+        z-index:100;
+    }
+
+    .my-float1{
+        margin-top:16px;
+    }
+
+    .my-float1 i:hover{
+        margin-top:16px;
+        color: #ecbcca;
+    }
 </style>
