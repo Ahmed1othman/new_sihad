aefb90e9d29a7b1b1ba9cb2cbbe75edfe8f91912
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-08-03 18:19:11 +0300

    whatsapp button

--- resources/views/layouts/website1/master.blade.php
+++ resources/views/layouts/website1/master.blade.php
@@ -2,12 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta http-equiv="content-security-policy"
-          content="default-src 'none'; script-src 'self';
-  connect-src 'self'; img-src 'self';
-  style-src 'self' 'sha256-CwE3Bg0VYQOIdNAkbB/Btdkhul49qZuwgNCMPgNY5zw=';" />
     <meta charset="utf-8"/>
-
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <link rel="icon" href="">
